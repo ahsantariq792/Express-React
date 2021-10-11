81badb2fcd87ba42519efbaa7bd9e6653ca11e8e
ahsantariq792 <tariqahsan63@gmail.com>
2021-10-12 00:49:43 +0500

    express

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const path = require('path')
+app.use('/', express.static(path.join(__dirname, 'todoapp/build')))
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+app.get('/profile', (req, res) => {
+    res.send('here is your profile')
+})
+app.post('/profile', (req, res) => {
+    res.send('profile is created')
+})
+
+
+app.put('/profile', (req, res) => {
+    res.send('profile is updated')
+})
+app.delete('/profile', (req, res) => {
+    res.send('profile is deleted')
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
